b33e627bd8af6206623e7443fe993d010ba3d0cb
MilanMareels <milan.mareels@gmail.com>
2025-02-07 10:05:20 +0100

    feat: added more info tekst to pakketten

--- src/data/serviceItems.ts
+++ src/data/serviceItems.ts
@@ -4,7 +4,7 @@ export const serviceItems = [
     src: ["/foto-1.jpg", "/foto-6.jpg"],
     title: "The disco Tent",
     description:
-      "Huur een unieke, opblaasbare discotheek voor je feest! Deze opvallende, ruime discotheek creëert meteen de perfecte party-sfeer. Optioneel kun je kiezen voor sfeervolle verlichting en een rookmachine, die het feest naar een hoger niveau tillen en de ruimte omtoveren tot een echte nightclub. Ideaal voor een verrassend en energiek evenement!",
+      "Huur een unieke, opblaasbare discotheek voor je feest! Deze opvallende, ruime discotheek creëert meteen de perfecte party-sfeer. Optioneel kun je kiezen voor sfeervolle verlichting en een rookmachine, die het feest naar een hoger niveau tillen en de ruimte omtoveren tot een echte nightclub. Ideaal voor een verrassend en energiek evenement! Niet te vergeten dat er ook altijd een van onze professionele fotografen mee aanwezig kunnen zijn op uw event! Ook kan elk pakket volledig gepersonaliseerd worden naar uw wensen.",
     price: "The disco Tent Huren met en zonder Toebehoren (400€-500€).",
   },
   {
@@ -12,7 +12,7 @@ export const serviceItems = [
     src: ["/foto-7.jpg", "/foto-2.jpg"],
     title: "Cocktail & cheers Bar",
     description:
-      "Laat de drankjes vloeien met onze professionele bar en barman! Dit pakket biedt je de mogelijkheid om een gepersonaliseerde bar te creëren die precies bij jouw feest past. De prijs wordt aangepast op basis van het aantal gewenste cocktails, Onze barman zorgt voor een vlotte service en een goede sfeer, zodat jouw gasten altijd een drankje binnen handbereik hebben!",
+      "Laat de drankjes vloeien met onze professionele bar en barman! Dit pakket biedt je de mogelijkheid om een gepersonaliseerde bar te creëren die precies bij jouw feest past. De prijs wordt aangepast op basis van het aantal gewenste cocktails, Onze barman zorgt voor een vlotte service en een goede sfeer, zodat jouw gasten altijd een drankje binnen handbereik hebben! Niet te vergeten dat er ook altijd een van onze professionele fotografen mee aanwezig kunnen zijn op uw event! Ook kan elk pakket volledig gepersonaliseerd worden naar uw wensen.",
     price: "Cocktail & cheers Bar + Barman (Prijs op aanvraag).",
   },
   {
@@ -20,7 +20,7 @@ export const serviceItems = [
     src: ["/foto-4.jpg", "/foto-9.jpg"],
     title: "Party tent vibes Bar",
     description:
-      "Voor een compleet feest met bar en eigen discotheek bieden wij het “party tent vibes pakket” aan. Je hebt de keuze uit een ruime, goed verzorgde tent waarin jouw gasten kunnen dansen en feesten, gecombineerd met een volledig uitgeruste bar. Dit pakket is ideaal voor feesten in de buitenlucht, waarbij je niet alleen wil zorgen voor een drankje, maar ook voor het gevoel van uitgaan.",
+      "Voor een compleet feest met bar en eigen discotheek bieden wij het “party tent vibes pakket” aan. Je hebt de keuze uit een ruime, goed verzorgde tent waarin jouw gasten kunnen dansen en feesten, gecombineerd met een volledig uitgeruste bar. Dit pakket is ideaal voor feesten in de buitenlucht, waarbij je niet alleen wil zorgen voor een drankje, maar ook voor het gevoel van uitgaan. Niet te vergeten dat er ook altijd een van onze professionele fotografen mee aanwezig kunnen zijn op uw event! Ook kan elk pakket volledig gepersonaliseerd worden naar uw wensen.",
     price: "Party tent vibes Bar + Tent (Prijs op aanvraag).",
   },
   {
@@ -28,7 +28,7 @@ export const serviceItems = [
     src: ["/foto-9.jpg", "/foto-7.jpg"],
     title: "Ultimate dance party Bar",
     description:
-      "Maak jouw feest compleet met dit uitgebreide pakket! Je krijgt niet alleen een professionele bar en een ruime dans tent, maar ook een ervaren DJ die de juiste muziek draait voor elk moment van de avond. Onze DJ komt met een volledige booth en is in staat om moeiteloos in te spelen op jouw thema of voorkeuren. Dit pakket zorgt voor de perfecte mix van drank, sfeer en muziek!",
+      "Maak jouw feest compleet met dit uitgebreide pakket! Je krijgt niet alleen een professionele bar en een ruime dans tent, maar ook een ervaren DJ die de juiste muziek draait voor elk moment van de avond. Onze DJ komt met een volledige booth en is in staat om moeiteloos in te spelen op jouw thema of voorkeuren. Dit pakket zorgt voor de perfecte mix van drank, sfeer en muziek! Niet te vergeten dat er ook altijd een van onze professionele fotografen mee aanwezig kunnen zijn op uw event! Ook kan elk pakket volledig gepersonaliseerd worden naar uw wensen.",
     price:
       "Ultimate dance party Bar + Tent + DJ incl. Booth (Prijs op aanvraag).",
   },
@@ -37,7 +37,7 @@ export const serviceItems = [
     src: ["/foto-8.jpg", "/foto-7.jpg"],
     title: "Exquisite feast",
     description:
-      "Voor een onvergetelijk feest met alles erop en eraan, is dit het ultieme pakket! Naast de bar, tent en DJ, komt er ook een foodtruck met een gevarieerd aanbod aan keukenopties. Van snacks tot volledige maaltijden, jouw gasten kunnen genieten van heerlijk eten terwijl ze dansen op de muziek van onze DJ. Dit alles zorgt voor een zorgeloze, complete ervaring waarbij je gasten zich geen moment hoeven te vervelen.",
+      "Voor een onvergetelijk feest met alles erop en eraan, is dit het ultieme pakket! Naast de bar, tent en DJ, komt er ook een foodtruck met een gevarieerd aanbod aan keukenopties. Van snacks tot volledige maaltijden, jouw gasten kunnen genieten van heerlijk eten terwijl ze dansen op de muziek van onze DJ. Dit alles zorgt voor een zorgeloze, complete ervaring waarbij je gasten zich geen moment hoeven te vervelen. Niet te vergeten dat er ook altijd een van onze professionele fotografen mee aanwezig kunnen zijn op uw event! Ook kan elk pakket volledig gepersonaliseerd worden naar uw wensen.",
     price:
       "Exquisite feast & beats Bar + Tent + DJ + Foodtruck (Prijs op aanvraag).",
   },
